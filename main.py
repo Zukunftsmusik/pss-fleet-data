8e681a88fffb6cfc777dc1738dca518baf0a708e
Zukunftsmusik <5472896+Zukunftsmusik@users.noreply.github.com>
2019-09-30 11:01:21 +0200

    Added cli parameters

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-
+import getopt
 import os
 import sys
 import time
@@ -36,15 +36,43 @@ def main():
             time.sleep(sleep_for_seconds)
 
 
-def init():
+def init(store_at_filesystem: bool = True, store_at_gdrive: bool = True):
+    if store_at_filesystem is not None:
+        util.dbg(f'Store at filesystem: {store_at_filesystem}')
+        settings.STORE_AT_FILESYSTEM = store_at_filesystem
+    if store_at_gdrive is not None:
+        util.dbg(f'Store at google drive: {store_at_filesystem}')
+        settings.STORE_AT_GDRIVE = store_at_gdrive
+
     if settings.FOLDERS_TO_BE_CREATED:
         for folder_name in settings.FOLDERS_TO_BE_CREATED:
             if not os.path.isdir(folder_name):
                 os.mkdir(folder_name)
 
 
+def print_help():
+    print(f'Usage: main.py -f <bool> -g <bool>')
+
+
 
 
 if __name__ == '__main__':
-    init()
+    args = sys.argv[1:]
+    store_at_filesystem = None
+    store_at_gdrive = None
+    try:
+        opts, args = getopt.getopt(sys.argv, 'hf:g:', 'fs=gdrive=')
+    except getopt.GetoptError:
+        print_help()
+    else:
+        for opt, arg in opts:
+            if opt == '-h':
+                print_help()
+                sys.exit()
+            elif opt == '-f' or opt == '--fs':
+                store_at_filesystem = bool(arg)
+            elif opt == '-g' or opt == '--gdrive':
+                store_at_gdrive = bool(arg)
+
+    init(store_at_filesystem=store_at_filesystem, store_at_gdrive=store_at_gdrive)
     main()
