17daf95f68d8f803d61e1c62e5906fd18ee98d18
Zukunftsmusik <5472896+Zukunftsmusik@users.noreply.github.com>
2020-11-22 18:48:08 +0100

    Refactored login

--- pss_login.py
+++ pss_login.py
@@ -3,11 +3,15 @@ import random
 import requests
 from xml.etree import ElementTree
 
-import settings
 import utility as util
 
 
 
+
+
+def create_device_checksum(device_key: str, device_type: str) -> str:
+    result = hashlib.md5((f'{device_key}{device_type}savysoda').encode('utf-8')).hexdigest()
+    return result
 
 
 def create_device_key() -> str:
@@ -28,22 +32,23 @@ def create_device_key() -> str:
     )
     return result
 
-def create_device_checksum(device_key: str) -> str:
-    result = hashlib.md5((f'{device_key}DeviceTypeMacsavysoda').encode('utf-8')).hexdigest()
-    return result
 
-def login(device_key: str = None, api_server: str = None) -> dict:
+def login(device_key: str = None, api_server: str = None, device_type: str = 'DeviceTypeMac') -> dict:
     if not device_key:
         device_key: str = create_device_key()
     if not api_server:
         api_server = util.get_api_server()
 
-    checksum = create_device_checksum(device_key)
-
-    path = f'UserService/DeviceLogin8?deviceKey={device_key}&isJailBroken=false&checksum={checksum}&deviceType=DeviceTypeMac&languageKey=en&advertisingkey=%22%22'
-
-    url = f'{api_server}/{path}'
-    data = requests.post(url).content.decode('utf-8')
+    params = {
+        'advertisingKey': '""',
+        'checksum': create_device_checksum(device_key, device_type),
+        'deviceKey': device_key,
+        'deviceType': device_type,
+        'isJailBroken': 'false',
+        'languageKey': 'en'
+    }
+    url = f'{api_server}/UserService/DeviceLogin8'
+    data = requests.post(url, params=params).content.decode('utf-8')
 
     xml = ElementTree.fromstring(data)
 
